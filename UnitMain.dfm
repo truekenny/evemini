8531325366b9eba8cbc83651a866642ffa79c32c
Sasha Yaroslavtsev <sasha@yaroslavtsev.net>
2019-07-22 20:48:14 +0300

    init

---
+++ UnitMain.dfm
@@ -0,0 +1,29 @@
+object FormMain: TFormMain
+  Left = 0
+  Top = 0
+  BorderStyle = bsNone
+  Caption = 'FormMain'
+  ClientHeight = 450
+  ClientWidth = 868
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  FormStyle = fsStayOnTop
+  OldCreateOrder = False
+  OnCreate = FormCreate
+  OnMouseDown = FormMouseDown
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Button1: TButton
+    Left = 32
+    Top = 16
+    Width = 75
+    Height = 25
+    Caption = 'Button1'
+    TabOrder = 0
+    OnClick = Button1Click
+  end
+end
---
+++ UnitMain.pas
@@ -0,0 +1,103 @@
+unit UnitMain;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TFormMain = class(TForm)
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormMouseDown(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Integer);
+  private
+    { Private declarations }
+    procedure explode(var a: array of string; Border, S: string);
+    function getHandle(): Cardinal;
+  public
+    { Public declarations }
+  end;
+
+var
+  FormMain: TFormMain;
+  capsuleerName: string;
+  gameX, gameY: Integer;
+
+
+implementation
+
+{$R *.dfm}
+
+function TFormMain.getHandle(): Cardinal;
+begin
+  Result := FindWindow('triuiScreen', PChar('EVE - ' + capsuleerName));
+end;
+
+procedure TFormMain.explode(var a: array of string; Border, S: string);
+var
+  S2: string;
+  i: Integer;
+begin
+  i  := 0;
+  S2 := S + Border;
+  repeat
+    a[i] := Copy(S2, 0,Pos(Border, S2) - 1);
+    Delete(S2, 1,Length(a[i] + Border));
+    Inc(i);
+  until S2 = '';
+end;
+
+procedure TFormMain.Button1Click(Sender: TObject);
+var
+  Desktop: HDC;
+begin
+  Desktop := GetWindowDC(getHandle());
+  try
+    BitBlt(FormMain.Canvas.Handle, 0, 0, FormMain.Width, FormMain.Height, Desktop, 100, 0, SRCCOPY);
+  finally
+    ReleaseDC(0, Desktop);
+  end;
+end;
+
+procedure TFormMain.FormCreate(Sender: TObject);
+var
+  index: Integer;
+  param, key, value: string;
+  pair: array of string;
+begin
+  SetLength(pair, 2);
+
+  for index := 1 to ParamCount do begin
+    param := ParamStr(index);
+    explode(pair, '=', param);
+
+    key := pair[0];
+    value := pair[1];
+
+    // --name="Lass Suicide" --form-x=200 --form-y=100 --form-width=400 --form-height=200 --game-x=300 --game-y=400
+
+    if key = '--name' then capsuleerName := value
+    else if key = '--form-x' then FormMain.Left := StrToInt(value)
+    else if key = '--form-y' then FormMain.Top := StrToInt(value)
+    else if key = '--form-width' then FormMain.Width := StrToInt(value)
+    else if key = '--form-height' then FormMain.Height := StrToInt(value)
+    else if key = '--game-x' then gameX := StrToInt(value)
+    else if key = '--game-y' then gameY := StrToInt(value);
+  end;
+
+  Left := 100;
+  Top := 100;
+end;
+
+procedure TFormMain.FormMouseDown(Sender: TObject; Button: TMouseButton;
+  Shift: TShiftState; X, Y: Integer);
+begin
+  if Button = mbLeft then SetForegroundWindow(getHandle)
+  else if Button = mbMiddle then Close();
+
+end;
+
+end.
